b5d67e41160641db1486ead72ca0b5f6930fd00a
Thilde <thilde.persson@gmail.com>
2025-03-20 21:59:01 +0100

    Tried to install react-router-dom but didnt succeed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import './App.css';
-
 import Home from './pages/Home';
-
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
-	return (
-	  <div className="App">
-		<Header />
-		<Home />
-		<Footer />
-	  </div>
-	);
-  }
+  return (
+    <Router>
+    	<Header />
+    	<Routes>
+    		<Route path="/" element={<Home />} />
+    	</Routes>
+    	<Footer />
+    </Router>
+  );
+}
 
 export default App;
